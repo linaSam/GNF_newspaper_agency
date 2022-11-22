ca0e8650ffa0bf74acb9ee5a529450bb14953e50
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-11-22 21:52:52 +0200

    Add debug-toolbar to requirements and settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "crispy_forms",
     "catalog"
 ]
@@ -58,7 +59,8 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware"
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "newspaper_agency.urls"
